fca73ce35359174aa7e2e7eb7c6be42671e2abfe
Nessa <la-nessa@mail.ru>
2022-12-23 01:00:02 +0300

    Ex2

---
+++ Ex2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите индекс строки: ");
+int k = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите индекс столбца: ");
+int  l = Convert.ToInt32(Console.ReadLine());
+
+int[,] arr = new int [3,4] {
+        {1, 2, 3, 4 },
+        {5, 6, 7, 8 },
+        {9, 10, 11, 12 }
+};
+
+if (k < 3 & l < 4)
+{
+    Console.Write("Искомый элемент: ");
+    Console.WriteLine(arr[k,l]);
+}
+else
+{
+    Console.WriteLine("Такой элемент не существует");
+}
